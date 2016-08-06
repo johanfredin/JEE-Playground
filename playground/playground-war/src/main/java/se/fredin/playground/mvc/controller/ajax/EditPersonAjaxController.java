9093959ceae899c0d7eee29e8ced916f431c7cca
Johan Fredin <johanfredin204@hotmail.com>
2016-08-06 13:42:38 +0200

    Now able to parse java object into json

--- playground/playground-war/src/main/java/se/fredin/playground/mvc/controller/ajax/EditPersonAjaxController.java
+++ playground/playground-war/src/main/java/se/fredin/playground/mvc/controller/ajax/EditPersonAjaxController.java
@@ -77,9 +77,15 @@ public class EditPersonAjaxController {
 		return new ModelAndView("redirect:/index.html");
 	}
 	
+//	@RequestMapping(value="/getMatchingFirstName", produces="application/json", method=RequestMethod.GET)
+//	public @ResponseBody List<String> getMatchingFirstName(@RequestParam("FIRST_NAMES") String firstName) {
+//		List<String> names = getPersonService().getAllFirstNamesLike(firstName);
+//		return names;
+//	}
+	
 	@RequestMapping(value="/getMatchingFirstName", produces="application/json", method=RequestMethod.GET)
-	public @ResponseBody List<String> getMatchingFirstName(@RequestParam("FIRST_NAMES") String firstName) {
-		List<String> names = getPersonService().getAllFirstNamesLike(firstName);
+	public @ResponseBody List<Person> getMatchingFirstNames(@RequestParam("FIRST_NAMES") String firstName) {
+		List<Person> names = getPersonService().getAllPersonsWithFirstNameLike(firstName);
 		return names;
 	}
 	
